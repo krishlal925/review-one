cb9ee434e272dadd506a89165fcef9e534d969bf
Neel <krishlal925@gmail.com>
2020-01-21 19:43:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+
+
+const [red, changeRed] = useState(0)
+const [blue, changeBlue] = useState(0)
+const [green, changeGreen] = useState(0)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      <div className="sliders">
+        Red <input type= 'range' min= '0' max= '255' step= '1' value = {red} name= "red" onChange={ (ev)=> changeRed(ev.target.value) }/>
+        Green <input type= 'range' min= '0' max= '255' step= '1' value = {green} name= "green" onChange={ (ev)=> changeGreen(ev.target.value)}/>
+        Blue  <input type= 'range' min= '0' max= '255' step= '1' value = {blue} name= "blue" onChange={ (ev)=> changeBlue(ev.target.value)}/>
+      </div>
+      <div className= "swatch" style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})`}}>
+
+      </div>
+    </main>
   );
 }
 
